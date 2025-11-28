34082bedc9c9805d2e1df23d6582e47abad0d201
Erik Nikulski <erik@nikulski.me>
2025-11-28 10:22:20 +0100

    Add option to include/remove agent direction from features

--- feature_extractor.py
+++ feature_extractor.py
@@ -39,20 +39,25 @@ class MinigridFeaturesExtractor(BaseFeaturesExtractor):
             max_rbf_grid_size: Optional[int] = None,
             rbf_sigma: Optional[float] = None,
             include_goal: Optional[bool] = None,
+            include_direction: Optional[bool] = None,
     ) -> None:
         self.config = Config()
 
         self.max_rbf_grid_size = self.config.get_or_raise(max_rbf_grid_size, 'feature_extractor', 'max_rbf_grid_size')
         self.rbf_sigma = self.config.get_or_raise(rbf_sigma, 'feature_extractor', 'rbf_sigma')
         self.include_goal = self.config.get_or_raise(include_goal, 'feature_extractor', 'include_goal')
+        self.include_direction = self.config.get_or_raise(include_direction, 'feature_extractor', 'include_direction')
 
         self.grid_size = observation_space['image'].shape[:-1]
         self.rbf_grid_size = min(*self.grid_size, self.max_rbf_grid_size)
 
+        # calculate feature dimension
         rbf_feature_dims = self.rbf_grid_size ** 2
         if self.include_goal:
             rbf_feature_dims *= 2
-        features_dim = rbf_feature_dims + self.AGENT_DIRECTION_DIMS + self.BIAS_DIM
+        direction_feature_dims = self.AGENT_DIRECTION_DIMS if self.include_direction else 0
+        features_dim = rbf_feature_dims + direction_feature_dims + self.BIAS_DIM
+
         super().__init__(observation_space, features_dim)
 
     def get_features(
@@ -79,7 +84,12 @@ class MinigridFeaturesExtractor(BaseFeaturesExtractor):
     def forward(self, observations: dict) -> np.ndarray:
         # agent features
         agent_pos = self.get_features(observations, self.AGENT_VALUE)
-        agent_direction = minigrid_constants.DIR_TO_VEC[observations['direction']]
+
+        # agent direction features
+        if self.include_direction:
+            agent_direction = minigrid_constants.DIR_TO_VEC[observations['direction']]
+        else:
+            agent_direction = np.array([])
 
         # goal features
         if self.include_goal:
